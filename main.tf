e3ef11fbc382e1362a398b73d6143dcb44a8ee1e
Santh Prakash Gangadhara <153313380+gspvsr@users.noreply.github.com>
2025-12-12 17:59:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "dev_instance" {
   count = 1
-  ami           = "ami-0ecb62995f68bb549"
+  ami           = "ami-0f5fcdfbd140e4ab7"
   instance_type = "t2.micro"
  
 
