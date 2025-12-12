7630a1075eaed2e4da489da67ac6fe3956d3b901
Santh Prakash Gangadhara <153313380+gspvsr@users.noreply.github.com>
2025-12-12 16:31:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev_instance" {
-count = 2
+  count = 2
   ami           = "ami-0ecb62995f68bb549"
   instance_type = "t2.micro"
   subnet_id     = "subnet-074ee1f2fb2076568"
