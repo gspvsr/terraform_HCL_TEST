4cdfb52d9768b58cc3dcfda56e6be4686ac34c96
Santh Prakash Gangadhara <153313380+gspvsr@users.noreply.github.com>
2025-12-12 21:12:16 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev_instance" {
   count = 1
-  ami           = "ami-0f5fcdfbd140e4ab7"
+  ami           = "ami-0ecb62995f68bb549"
   instance_type = "t2.micro"
  
 
